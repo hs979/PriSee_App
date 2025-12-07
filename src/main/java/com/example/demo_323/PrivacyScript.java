c7c013b5f65ffbf247086b0d1d11e7ae5e2e5944
AllForYann <chenxy979@mail2.sysu.edu.cn>
2025-12-07 17:53:26 +0800

    Delete src directory

--- src/main/java/com/example/demo_323/PrivacyScript.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo_323;
-
-import java.util.List;
-
-public class PrivacyScript  {
-    public List<ActionStep> steps;     // 进入路径步骤
-    public List<ActionStep> switches;  // 需要关闭的开关列表
-}
